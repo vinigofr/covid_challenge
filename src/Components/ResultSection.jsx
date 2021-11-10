098cc284a3a1a69c8753ad44b9132a9ecb9575ee
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 20:08:49 -0300

    Remove conteudo desnecessario para refatoraçao

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-function ResultSection() {
+function ResultSection({ data }) {
   return (
     <div className="result-section">
-      Resultados aqui
+      <h1>Faça uma consulta acima, os resultados aparecerão abaixo:</h1>
+      {console.log(data.filter((item, index) => index < 1))}
     </div>
   );
 }
