313e40b2b7a68b85b642e3068e3fb75ab0971b68
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 16:47:33 -0300

    Adiciona componente para exibiçao dos resultados

---
+++ src/Components/ResultSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ResultSection() {
+  return (
+    <div className="result-section">
+      Resultados aqui
+    </div>
+  );
+}
+
+export default ResultSection;
