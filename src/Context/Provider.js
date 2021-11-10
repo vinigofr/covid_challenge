84d3d322988594a358a1a35f4e9ad0ca67bd59e7
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 17:09:02 -0300

    Adiciona ao context um estado para armazenagem de dados

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
-  const context = {};
+  const [data, setData] = React.useState([]);
+
+  const context = {
+    data, setData,
+  };
+
   return (
     <Context.Provider value={context}>
       {children}
