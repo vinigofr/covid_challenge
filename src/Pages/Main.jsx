9534ec83693fa6ac89745c6d149a6d666be93c41
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 16:54:57 -0300

    Adiciona header com componente Main

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+// Components \/
+import Header from '../Components/Header';
+// Components /\
+
 function Main() {
   return (
     <div>
+      <Header />
       <h1>aaa</h1>
     </div>
   );
