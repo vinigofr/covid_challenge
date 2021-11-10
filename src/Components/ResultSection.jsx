7233374c66f3f8d7a5a1d7df7df30e517f39baaf
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 20:17:57 -0300

    Adiciona componente ao ResultSection.jsx

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import SearchOptions from './SearchOptions';
 
 function ResultSection({ data }) {
   return (
     <div className="result-section">
+      <SearchOptions />
       <h1>Faça uma consulta acima, os resultados aparecerão abaixo:</h1>
       {console.log(data.filter((item, index) => index < 1))}
     </div>
