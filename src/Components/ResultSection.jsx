d362e012999a2f7254aeee68b3c4cbde42d3dffc
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 20:45:01 -0300

    Faz com que os itens renderizem corretamentee

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -5,12 +5,21 @@ import Context from '../Context/Context';
 
 function ResultSection({ data }) {
   const { currentDay } = useContext(Context);
+  const itemsToRender = data.filter((_item, index) => index < currentDay);
 
   return (
     <div className="result-section">
       <SearchOptions maxDays={data.length} />
       <h1>Faça uma consulta acima, os resultados aparecerão abaixo:</h1>
-      {console.log(data.filter((item, index) => index < currentDay))}
+      <div>
+        {itemsToRender.map(({ date, total_cases: totalCases, new_cases: newCases }) => (
+          <div className="case-card">
+            <h2>{`Data: ${date}`}</h2>
+            <p>{`Total de casos: ${totalCases}`}</p>
+            <p>{`Novos casos: ${newCases}`}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
