328e5c4597b3d60031315b651c26826e85dfa27a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-09 22:11:32 -0300

    Implementa componente no App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Provider from './Context/Provider';
+import Main from './Pages/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Provider>
         <Switch>
-          <Route exact path="/" />
+          <Route exact path="/" component={Main} />
         </Switch>
       </Provider>
     </BrowserRouter>
