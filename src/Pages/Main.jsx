5b98fde79c1cd36b6df55a0ecb1b7b3a7a84c920
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 20:09:37 -0300

    Faz com que receba os dados armazenados no contexto

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -3,17 +3,11 @@ import Context from '../Context/Context';
 
 // Components \/
 import Header from '../Components/Header';
-import SearchButton from '../Components/SearchButton';
-import DayInput from '../Components/DayInput';
 import ResultSection from '../Components/ResultSection';
 // Components /\
 
 function Main() {
-  const {
-    data,
-    setData,
-  } = React.useContext(Context);
-
+  const { data, setData } = React.useContext(Context);
   const [load, setLoad] = React.useState(true);
 
   React.useEffect(async () => {
@@ -24,18 +18,13 @@ function Main() {
         setData(OwidData.OWID_WRL.data);
         setLoad(false);
       })
-      // .then((OwidData) => console.log(OwidData.OWID_WRL.data))
       .catch((e) => console.log(e));
   }, []);
 
   return (
     <div>
       <Header />
-      <div className="search-section">
-        <SearchButton />
-        <DayInput maxDays={data.length} />
-      </div>
-      { load ? 'Baixando os dados, aguarde...' : <ResultSection /> }
+      { load ? 'Baixando os dados, aguarde...' : <ResultSection data={data} /> }
     </div>
   );
 }
