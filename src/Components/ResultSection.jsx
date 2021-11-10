8818f549bd8ec686befc88d1e1be6b62ff62dcbb
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 20:21:24 -0300

    Faz com que ao clicar no botao de 'Buscar' os resultados sejam filtrados

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 import SearchOptions from './SearchOptions';
+import Context from '../Context/Context';
 
 function ResultSection({ data }) {
+  const { currentDay } = useContext(Context);
+
   return (
     <div className="result-section">
       <SearchOptions maxDays={data.length} />
       <h1>Faça uma consulta acima, os resultados aparecerão abaixo:</h1>
-      {console.log(data.filter((item, index) => index < 1))}
+      {console.log(data.filter((item, index) => index < currentDay))}
     </div>
   );
 }
