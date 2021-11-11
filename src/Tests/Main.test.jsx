3d306ddcb9f7b9f31504e1e68e2a325ea258553d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-11 01:29:29 -0300

    Implemente Main.test.jsx

---
+++ src/Tests/Main.test.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import React from 'react';
+import {
+  act,
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+  waitForElementToBeRemoved,
+  waitFor,
+} from '@testing-library/react';
+import App from '../App';
+import Main from '../Pages/Main';
+import covidCasesData from './data.json';
+
+describe('Testa o componente Main.jsx, que faz parte da tela inicial', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: () => Promise.resolve({ OWID_WRL: { data: covidCasesData } }),
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('Verifica se na página tem um header nível 1 com o texto "Como estão os casos de COVID-19 no mundo?"', async () => {
+    await act(async () => { render(<App />); });
+    const h1 = screen.getAllByRole('heading', { level: 1 });
+    expect(h1[0]).toBeInTheDocument();
+    expect(h1[0]).toHaveTextContent('Como estão os casos de COVID-19 no mundo?');
+  });
+
+  it('Verifica se após o retorno da API, uma tabela é renderizada', async () => {
+    await act(async () => { render(<App />); });
+    expect(global.fetch).toHaveBeenCalled();
+    const table = screen.getByRole('table');
+    await waitFor(() => expect(table).toBeInTheDocument());
+  });
+
+  it('Verifica se ao haver um erro na requisição, um alerta é disparado', async () => {
+    jest.spyOn(global, 'fetch').mockRejectedValue({ message: 'Erro ao carregar dados' });
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+    await act(async () => { render(<App />); });
+    expect(global.fetch).toHaveBeenCalled();
+    expect(global.alert).toHaveBeenCalled();
+  });
+});
