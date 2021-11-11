45dc1784acaef459a7ff9ae1537d34afe7809c93
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-10 21:20:56 -0300

    Implementa resultados no formato de tabela

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -12,13 +12,23 @@ function ResultSection({ data }) {
       <SearchOptions maxDays={data.length} />
       <h1>Faça uma consulta acima, os resultados aparecerão abaixo:</h1>
       <div>
-        {itemsToRender.map(({ date, total_cases: totalCases, new_cases: newCases }) => (
-          <div className="case-card">
-            <h2>{`Data: ${date}`}</h2>
-            <p>{`Total de casos: ${totalCases}`}</p>
-            <p>{`Novos casos: ${newCases}`}</p>
-          </div>
-        ))}
+        <table>
+          <tr>
+            <th>N. do dia</th>
+            <th>Data</th>
+            <th>Casos</th>
+            <th>Novos</th>
+          </tr>
+          {itemsToRender
+            .map(({ date, total_cases: totalCases, new_cases: newCases }, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{date}</td>
+                <td>{totalCases}</td>
+                <td>{newCases}</td>
+              </tr>
+            ))}
+        </table>
       </div>
     </div>
   );
