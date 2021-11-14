a79c6bf9a29332fac280dcb3fe69cc675199a303
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-14 13:16:57 -0300

    Implementa toda a logica necessaria para retorno de previsao no arquivo caseAverage.js

--- src/Helpers/caseAverage.js
+++ src/Helpers/caseAverage.js
@@ -1,28 +1,26 @@
 function main(covidCases, MAX_DAYS) {
   const newPrevist = [];
-  const copyCases = covidCases;
-
-  console.log(covidCases);
-  console.log(MAX_DAYS);
+  const copyCases = [...covidCases];
 
   const getTheAverage = () => {
     // Obtém os últimos 14 dias de casos
     const rangeOfCases = copyCases.slice(copyCases.length - 14);
 
-    // Retorna a média dos casos para a constante casesAvarage
-    const casesAvarage = rangeOfCases.reduce((acc, curr) => acc + curr, 0) / rangeOfCases.length;
-
-    // Manda para o array newPrevist a última previsão realizada
-    newPrevist.push(casesAvarage);
-
+    // Retorna a média dos casos para a constante casesAverage
+    const casesAverage = Math.flo(rangeOfCases
+      .reduce((acc, curr) => acc + curr.new_cases, 0) / rangeOfCases.length);
+    
     // Manda o valor gerado em rangeOfCases para o array copyCases, para calculos futuros
-    copyCases.push(casesAvarage);
+    copyCases.push({ new_cases: casesAverage });
+    return casesAverage;
   };
 
   for (let i = 0; i < MAX_DAYS; i += 1) {
     const average = getTheAverage();
     newPrevist.push(average);
+    
   }
+  console.log(copyCases);
   return newPrevist;
 }
 export default main;
