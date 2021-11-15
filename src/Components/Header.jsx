cf6f4d1313ce33d72555c70dbde356a1707f9f99
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-14 23:35:22 -0300

    Muda texto do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1 className="title-header">Como estão os casos de COVID-19 no mundo? </h1>
+      <h1 className="title-header">Como estarão os casos de COVID-19 no mundo? </h1>
       <h3 className="subtitle-header">Desafio D3 Company</h3>
     </header>
   );
