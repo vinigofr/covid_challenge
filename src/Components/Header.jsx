42c36e9d54439bf23f100bbbf659cf1371534efe
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-14 23:47:02 -0300

    Adiciona fonte ao header, para quem quiser acessar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+const URL = 'https://ourworldindata.org/coronavirus';
+const SITE_NAME = 'Our World in Data';
+
 function Header() {
   return (
     <header>
-      <h1 className="title-header">Como estarão os casos de COVID-19 no mundo? </h1>
-      <h3 className="subtitle-header">Desafio D3 Company</h3>
+      <div>
+        <h1 className="title-header">Como estarão os casos de COVID-19 no mundo? </h1>
+        <h3 className="subtitle-header">Desafio D3 Company</h3>
+      </div>
+      <div>
+        <p>
+          Esta aplicação é alimentada por:
+          <a href={URL} target="_blank" rel="noreferrer">{` ${SITE_NAME}`}</a>
+        </p>
+      </div>
     </header>
   );
 }
